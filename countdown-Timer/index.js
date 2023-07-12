21e2f06fb0819ec33b9c97407b1a2703754c38a9
oshilim-chukwudi <chukwudioshilim@gmail.com>
2023-07-11 22:02:09 -0400

    final comit -project 1

--- countdown-Timer/index.js
+++ countdown-Timer/index.js
@@ -1,4 +1,33 @@
 const daysEl = document.getElementById("days");
-let hoursEl = document.getElementById("hours");
-let minutesEl = document.getElementById("minutes");
-const sec = document.getElementById("seconds");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newyears = "1 January 2024";
+
+function countdown() {
+  const newyearsdate = new Date(newyears);
+  const currentdate = new Date();
+
+  const totalseconds = (newyearsdate - currentdate) / 1000;
+
+  const days = Math.floor(totalseconds / 3600 / 24);
+  const hours = Math.floor(totalseconds / 3600) % 24;
+  const minutes = Math.floor(totalseconds / 60) % 60;
+  const seconds = Math.floor(totalseconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = format(hours);
+  minutesEl.innerHTML = format(minutes);
+  secondsEl.innerHTML = format(seconds);
+}
+
+function format(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+// Initial call
+countdown();
+
+// Update countdown every second
+setInterval(countdown, 1000);
