a6dab1cb073adb7bed272ac1460f50f6df26b9a8
oshilim-chukwudi <chukwudioshilim@gmail.com>
2023-07-11 20:05:48 -0400

    initial comit

---
+++ countdown-Timer/index.js
@@ -0,0 +1,4 @@
+const daysEl = document.getElementById("days");
+let hoursEl = document.getElementById("hours");
+let minutesEl = document.getElementById("minutes");
+const sec = document.getElementById("seconds");
